f7a9cac790d9c4e35e7b3781240fd378bce4a1ed
vaxad <varadprabhu111@gmail.com>
2023-08-03 19:13:38 +0530

    cors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ connectMongo();
 
 const express = require('express');
 const app = express();
+var cors = require('cors')
 const port = 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/blogs', require('./routes/blogs'));
 app.use('/api/user', require('./routes/user'));
